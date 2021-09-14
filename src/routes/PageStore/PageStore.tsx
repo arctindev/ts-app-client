599fc155db2deb82e9a297a9276c99d16843775b
arctindev <arctindev@gmail.com>
2021-09-14 19:26:31 +0200

    Added custom scrollbar for light and dark theme

--- src/routes/PageStore/PageStore.tsx
+++ src/routes/PageStore/PageStore.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import styles from './PageStore.module.scss';
 import { Title } from '../../components/Title/Title';
 import ServiceList from '../../components/ServiceList/ServiceList';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
+import { RootStateOrAny, useSelector } from 'react-redux';
 
 const PageStore = (): JSX.Element => {
+  const ref:any = useRef();
+  const darkMode = useSelector((state: RootStateOrAny) => state.darkMode);
+
+  useEffect(() => {
+    const scroll = ref.current.el.childNodes[2].childNodes[0];
+    darkMode?
+    scroll.className += ` ${styles.DarkScrollColor}`
+    :
+    scroll.className += ` ${styles.ScrollColor}`
+
+    console.log('performanceCheck')
+  },[darkMode]);
+
   return (
-    <div className={styles.Wrapper}>
-      <Title titleText="Store" />
-      <ServiceList />
+    <div>
+      <SimpleBar ref={ref} className={styles.Wrapper}>
+        <Title titleText="Store" />
+        <ServiceList />
+      </SimpleBar>
     </div>
   );
 };
